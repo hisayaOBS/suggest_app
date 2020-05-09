2f52524817fccb47cd27ed3ab237c10b92443858
hisayaOBS <h.miyoshi948@gmail.com>
2020-05-10 08:36:32 +0900

    静的ページ生成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def full_title(title = '')
+    base_title = 'SUGGEST!!'
+    if title.empty?
+      base_title
+    else
+      title + ' | ' + base_title
+    end
+  end
+
+  def active?(action_name)
+    return 'active' if action_name == params[:action]
+  end
 end
